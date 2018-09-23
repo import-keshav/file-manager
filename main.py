03dace5c8d9d642ef3edfc109e7cfb7a180faddc
import_keshav <keshavbathla2017@gmail.com>
2018-09-24 01:50:33 +0545

    added new features

--- main.py
+++ main.py
@@ -7,26 +7,28 @@ def enter_dir(path,root):
 		root.destroy()
 		root=Tk()
 		root.title(path)
-		
+
 		current_path=HOME_PATH + '/' + path
 		os.chdir(current_path)
-		
+
 		files=os.listdir(current_path)
-		print(files,end="\n")
+		btn=[]
+
 		row_num=0
 		column_num=1
-				
-		for i in files:
-			if(column_num%5!=0):
-				button=Button(root,text=str(i),height=0,width=0,font=("Helvetica",30),bg="powderblue",command=lambda: enter_dir(str(i),root))
-				button.grid(row=row_num,column=column_num,sticky=EW,padx=2,pady=1)
+		for i in range(0,len(files)):
+			if(column_num%5>0 and column_num%5<=4):
+				btn.append(Button(root,text=str(files[i]),height=0,width=0,font=("Helvetica",10),bg="powderblue",command=lambda c=i: enter_dir(btn[c].cget('text'),root)))
+				btn[i].pack()
 				column_num+=1
 			else:
+				btn.append(Button(root,text=str(files[i]),height=0,width=0,font=("Helvetica",10),bg="powderblue",command=lambda c=i: enter_dir(btn[c].cget('text'),root)))
+				btn[i].pack()
 				row_num+=1
 				column_num=1
-	
+
 		root.mainloop()
-		
+
 def main():
 	os.chdir(HOME_PATH)
 	files=os.listdir(HOME_PATH)
@@ -34,21 +36,25 @@ def main():
 
 	root=Tk()
 	root.title("Home")
-	photo=PhotoImage(file="image.png")
+	#photo=PhotoImage(file='image.png')
+
 	row_num=0
 	column_num=1
-	
-	for i in files:
-		if(column_num%5!=0):
-			button=Button(root,text=str(i),height=0,width=0,font=("Helvetica",30),bg="powderblue",command=lambda: enter_dir(str(i),root))
-			button.image=photo
-			button.grid(row=row_num,column=column_num,sticky=EW,padx=2,pady=1)
+	btn=[]
+
+	print(len(files))
+	for i in range(0,len(files)):
+		if(column_num%5>0 and column_num%5<=4):
+			btn.append(Button(root,text=str(files[i]),height=0,width=0,font=("Helvetica",10),bg="powderblue",command=lambda c=i: enter_dir(btn[c].cget('text'),root)))
+			#button.image=photo
+			btn[i].grid(row=row_num,column=column_num,sticky=EW,padx=2,pady=1)
 			column_num+=1
 		else:
+			btn.append(Button(root,text=str(files[i]),height=0,width=0,font=("Helvetica",10),bg="powderblue",command=lambda c=i: enter_dir(btn[c].cget('text'),root)))
+			#button.image=photo
+			btn[i].grid(row=row_num,column=column_num,sticky=EW,padx=2,pady=1)
 			row_num+=1
 			column_num=1
-			
 	root.mainloop()
 
 main()
-
